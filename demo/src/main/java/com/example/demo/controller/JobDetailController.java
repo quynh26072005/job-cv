813ea1884b40eb04dbd1b203b52755491e1ce1a0
QuanHuynh123 <huynhminhquan07072002@gmail.com>
2025-05-18 21:53:06 +0700

    update job

--- demo/src/main/java/com/example/demo/controller/JobDetailController.java
+++ demo/src/main/java/com/example/demo/controller/JobDetailController.java
@@ -1,4 +1,14 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class JobDetailController {
+
+    @GetMapping("/jobDetail")
+    public String jobDetailPage(Model model) {
+        return "jobDetail";
+    }
 }
